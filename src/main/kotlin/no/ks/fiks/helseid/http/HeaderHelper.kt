c7434b4baefc67441f80de42830d72d55e57fe86
Anders Flemmen <anders.flemmen@ks.no>
2025-04-24 09:13:05 +0200

    Tweak constants

--- src/main/kotlin/no/ks/fiks/helseid/http/HeaderHelper.kt
+++ src/main/kotlin/no/ks/fiks/helseid/http/HeaderHelper.kt
@@ -1,10 +1,14 @@
 package no.ks.fiks.helseid.http
 
-private const val HEADER_AUTHORIZATION = "Authorization"
-private const val PREFIX_BEARER = "Bearer"
+private object Header {
+    const val AUTHORIZATION = "Authorization"
+    const val DPOP = "DPoP"
+}
 
-private const val HEADER_DPOP = "DPoP"
-private const val PREFIX_DPOP = "DPoP"
+private object Scheme {
+    const val BEARER = "Bearer"
+    const val DPOP = "DPoP"
+}
 
 object HeaderHelper {
 
@@ -12,20 +16,20 @@ object HeaderHelper {
         accessToken: String,
         setHeaderFunction: (headerName: String, headerValue: String) -> Any,
     ) {
-        setHeaderFunction.invoke(HEADER_AUTHORIZATION, buildAuthorizationHeaderValue(accessToken))
+        setHeaderFunction.invoke(Header.AUTHORIZATION, buildAuthorizationHeaderValue(accessToken))
     }
 
-    private fun buildAuthorizationHeaderValue(accessToken: String) = "$PREFIX_BEARER $accessToken"
+    private fun buildAuthorizationHeaderValue(accessToken: String) = "${Scheme.BEARER} $accessToken"
 
     fun setHeaders(
         accessToken: String,
         dpopProof: String,
         setHeaderFunction: (headerName: String, headerValue: String) -> Any,
     ) {
-        setHeaderFunction.invoke(HEADER_AUTHORIZATION, buildDpopAuthorizationHeaderValue(accessToken))
-        setHeaderFunction.invoke(HEADER_DPOP, dpopProof)
+        setHeaderFunction.invoke(Header.AUTHORIZATION, buildDpopAuthorizationHeaderValue(accessToken))
+        setHeaderFunction.invoke(Header.DPOP, dpopProof)
     }
 
-    private fun buildDpopAuthorizationHeaderValue(accessToken: String) = "$PREFIX_DPOP $accessToken"
+    private fun buildDpopAuthorizationHeaderValue(accessToken: String) = "${Scheme.DPOP} $accessToken"
 
 }
