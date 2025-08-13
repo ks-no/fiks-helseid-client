09be795a5555b7fd78feee2e0a8bb03a036e3b1c
Anders Flemmen <anders.flemmen@ks.no>
2025-08-13 13:46:59 +0200

    Include body with limit in HttpException message

--- src/main/kotlin/no/ks/fiks/helseid/http/HttpException.kt
+++ src/main/kotlin/no/ks/fiks/helseid/http/HttpException.kt
@@ -1,3 +1,3 @@
 package no.ks.fiks.helseid.http
 
-class HttpException(val status: Int, val body: String) : RuntimeException("HTTP request failed with status $status")
+class HttpException(val status: Int, val body: String) : RuntimeException("HTTP request failed with status $status: ${body.take(400)}")
